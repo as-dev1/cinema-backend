c2946dd7945bd39bcf2d96ce0fa427f90f2cc4a7
as-dev <tst.1a2a3@gmail.com>
2024-10-08 19:30:57 +0200

    add little improvement in projection controller

--- src/controllers/projection.controller.js
+++ src/controllers/projection.controller.js
@@ -2,7 +2,11 @@ import Projection from "../models/projection.model.js";
 
 export const getAllProjections = async (req, res) => {
     try {
-        const projections = await Projection.find().populate("movie").sort({ starting_at: 1 });
+        const projections = await Projection.find()
+            .populate("movie", "image_url name genre duration")
+            .where("starting_at")
+            .gte(new Date())
+            .sort({ starting_at: 1 });
         res.json(projections);
     } catch (error) {
         res.status(500).json({ message: "Internal server error" });
