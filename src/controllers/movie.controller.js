1c545a86ffc749e13f8c2d1ec077a3f5973eac87
as-dev <tst.1a2a3@gmail.com>
2024-10-09 11:16:41 +0200

    add get movie by id route

--- src/controllers/movie.controller.js
+++ src/controllers/movie.controller.js
@@ -9,6 +9,16 @@ export const getAllMovies = async (req, res) => {
     }
 };
 
+export const getMovieById = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const movie = await Movie.findById(id);
+        res.json(movie);
+    } catch (error) {
+        res.status(500).json({ message: "Internal server error" });
+    }
+};
+
 // just for easier adding in mongodb
 export const createMovie = async (req, res) => {
     const movie = new Movie(req.body);
--- src/routes/movie.route.js
+++ src/routes/movie.route.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { getAllMovies, createMovie } from "../controllers/movie.controller.js";
+import { getAllMovies, getMovieById, createMovie } from "../controllers/movie.controller.js";
 
 const router = Router();
 
 router.get("/", getAllMovies);
+router.get("/:id", getMovieById);
 router.post("/", createMovie);
 
 export default router;
